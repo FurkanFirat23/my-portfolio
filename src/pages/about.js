765af13cc65dc22fa06c730757f0b116b0527c93
Furkan Fırat <furkan.0096@gmail.com>
2023-04-22 15:36:43 +0300

    added some animation to numbers

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,8 +2,32 @@ import AnimatedText from "@/components/AnimatedText";
 import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import pphd from "../../public/images/profile/pphd.jpg";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { damping: 100, stiffness: 100 });
+  const isInView = useInView(ref, { once: true });
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
+
+  useEffect(() => {
+    springValue.on("change", (v) => {
+      if (ref.current && v.toFixed(0) <= value) {
+        ref.current.textContent = v.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
+
+  return <span ref={ref}></span>;
+};
+
 const about = () => {
   return (
     <>
@@ -59,11 +83,39 @@ const about = () => {
             </div>
 
             <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 ">
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark " />
               <Image
                 src={pphd}
                 alt="FurkanF"
                 className="w-full h-auto rounded-2xl"
               />
+            </div>
+
+            <div className="col-span-2 flex flex-col items-end justify-between ">
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-5xl font-bold">
+                  <AnimatedNumbers value={10} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  satisfied clients
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-5xl font-bold">
+                  <AnimatedNumbers value={5} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  projects completed
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-5xl font-bold">
+                  <AnimatedNumbers value={1} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  years of experience
+                </h2>
+              </div>
             </div>
           </div>
         </Layout>
