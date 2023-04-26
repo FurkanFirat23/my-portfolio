7eed6bac15ac67c5862422bc35709abb0fd38d62
Furkan Fırat <furkan.0096@gmail.com>
2023-04-26 20:52:32 +0300

    projects page created

---
+++ src/pages/projects.js
@@ -0,0 +1,132 @@
+import AnimatedText from "@/components/AnimatedText";
+import { GithubIcon } from "@/components/Icons";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import project1 from "../../public/images/projects/KanbanBoard.png";
+const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+  return (
+    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-8">
+      <Link
+        href={link}
+        target="_blank"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6 ">
+        <span className="text-primary font-medium text-xl ">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2 "
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
+        </Link>
+        <p className="my-2 font-medium text-dark ">{summary}</p>
+        <div className="mt-2 flex items-center ">
+          <Link href={github} target="_blank" className="w-10 ">
+            <GithubIcon />
+          </Link>
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark duration-500 ease-in-out"
+          >
+            Visit Project Site
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+const Project = ({ title, type, img, link, github }) => {
+  return (
+    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light mt-4 relative ">
+      <Link
+        href={link}
+        target="_blank"
+        className="w-full cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+
+      <div className="w-full flex flex-col items-start justify-between pl-6 ">
+        <span className="text-primary font-medium text-xl ">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2 "
+        >
+          <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
+        </Link>
+
+        <div className="mt-2 flex items-center ">
+          <Link href={github} target="_blank" className="w-10 ">
+            <GithubIcon />
+          </Link>
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark duration-500 ease-in-out"
+          >
+            Visit
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+const projects = () => {
+  return (
+    <>
+      <Head>
+        <title>Furkan FIRAT | Projects Page</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <main className="w-full mb-16 flex flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText
+            text="Innovative Ventures: A Showcase of My Projects"
+            className="mb-16"
+          />
+          <div className="grid grid-col-12 gap-24 ">
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Kanban Board"
+                img={project1}
+                summary="Platform offers three main features to make task management easier and more efficient for users: Drag-n-Drop for easy rearrangement of tasks, Delete and Add for flexibility in task management, and Update/Edit for making changes to tasks without losing progress. These features provide a seamless experience for users to keep everything organized and running smoothly.
+            "
+                link="/kanban-board-ruddy.vercel.app/"
+                github="/https://github.com/FurkanFirat23/Kanban-Board"
+                type="Featured Project"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="RealtorFF"
+                img={project1}
+                summary="Platform offers three main features to make task management easier and more efficient for users: Drag-n-Drop for easy rearrangement of tasks, Delete and Add for flexibility in task management, and Update/Edit for making changes to tasks without losing progress. These features provide a seamless experience for users to keep everything organized and running smoothly.
+            "
+                link="/kanban-board-ruddy.vercel.app/"
+                github="/https://github.com/FurkanFirat23/Kanban-Board"
+                type="Featured Project"
+              />
+            </div>
+            <div className="col-span-6">Project-2</div>
+            <div className="col-span-12">Featured Project</div>
+            <div className="col-span-6">Project-3</div>
+            <div className="col-span-6">Project-4</div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default projects;
