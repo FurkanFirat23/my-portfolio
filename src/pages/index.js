bfc2d84ef6a98e97c3cc8dca48ec92bed93c1915
Furkan Fırat <furkan.0096@gmail.com>
2023-05-04 13:53:22 +0300

    margin left added on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
               />
             </div>
-            <div className="w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center">
+            <div className="w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center ml-6">
               <AnimatedText
                 text="Engineering Interfaces That Spark Joy and Engagement!"
                 className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
